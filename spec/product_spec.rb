f666ed70c89aa4d35c6e66f015768506d0b62716
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-24 19:08:03 +0000

    rspec product test 5/5 and cinstruction comments from compass

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,13 +4,16 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
+    # each example will needs its own @category created and then @product initialized with that category
     it "will save successfully with all four fields set" do
       @category = Category.new
+      # initial example that ensures that a product with all four fields set will indeed save successfully
       @product = Product.new(name: "Kyra", price: 1000, quantity: 2, category: @category)
     end
 
     it "will fail when name is not present" do 
       @category = Category.new
+      # Set all fields to a value but the validation field being tested to nil
       @product = Product.new(name: nil, price: 1000, quantity: 2, category: @category)
     end
 
@@ -26,10 +29,9 @@ RSpec.describe Product, type: :model do
 
     it "will fail when category is not present" do 
       @category = Category.new
-      @product = Product.new(name: , price: , quantity: , category: )
+      @product = Product.new(name: "Kyra", price: 1000, quantity: 2, category: nil)
     end
 
   end
-
 
 end
