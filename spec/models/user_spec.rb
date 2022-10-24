62cf13b95ba0ecb7d95eed2be928340b578b110b
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-24 21:24:09 +0000

    rspec user test #1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "will save successfully with all four fields set" do
+      @user = User.new(name: "James", email: "hello@example.com", password: "123", password_confirmation: "123")
+      @user.save
+      expect(@user).to be_valid
+    end
+
+  end
+
 end
