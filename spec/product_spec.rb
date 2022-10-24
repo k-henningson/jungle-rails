de5a154f3f7e079638721e5f9c26a995922af3e6
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-24 19:01:54 +0000

    rspec product test 1/5

---
+++ spec/product_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    it "will save successfully with all four fields set" do
+      @category = Category.new
+      @product = Product.new(name: "Kyra", price: 1000, quantity: 2, category: @category)
+    end
+
+    it "will fail when name is not present" do 
+      @category = Category.new
+      @product = Product.new(name: , price: , quantity: , category: )
+    end
+
+    it "will fail when price is not present" do 
+      @category = Category.new
+      @product = Product.new(name: , price: , quantity: , category: )
+    end
+
+    it "will fail when quantity is not present" do 
+      @category = Category.new
+      @product = Product.new(name: , price: , quantity: , category: )
+    end
+
+    it "will fail when category is not present" do 
+      @category = Category.new
+      @product = Product.new(name: , price: , quantity: , category: )
+    end
+
+  end
+
+
+end
