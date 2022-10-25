255e8ba55a4bc44e0d5d9dcb5063b76491ed580a
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-25 02:09:31 +0000

    change name to first_name, last_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
 
-    it "will save successfully with all four fields set" do
-      @user = User.new(name: "James", email: "hello@example.com", password: "123", password_confirmation: "123")
+    it "will save successfully with all five fields set" do
+      @user = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "123", password_confirmation: "123")
       @user.save
       expect(@user).to be_valid
     end
@@ -13,19 +13,19 @@ RSpec.describe User, type: :model do
       @user = User.new(email: "hello@example.com", password: "123", password_confirmation: "123")
       @user.save
       puts @user.errors.full_messages
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
 
     it "will fail when email is not present" do 
-      @user = User.new(name: "James", password: "123", password_confirmation: "123")
+      @user = User.new(first_name: "James", last_name: "Smith", password: "123", password_confirmation: "123")
       @user.save
       puts @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it "will fail when password is not present" do 
-      @user = User.new(name: "James", email: "hello@example.com", password_confirmation: "123")
+      @user = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password_confirmation: "123")
       @user.save
       puts @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Password can't be blank")
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
 
 
     it "will fail when password confirmation is not present" do 
-      @user = User.new(name: "James", email: "hello@example.com", password: "123")
+      @user = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "123")
       @user.save
       puts @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
@@ -41,23 +41,23 @@ RSpec.describe User, type: :model do
 
 
     it "will fail when password and password confirmation do not match" do 
-      @user = User.new(name: "James", email: "hello@example.com", password: "123", password_confirmation: "456")
+      @user = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "123", password_confirmation: "456")
       @user.save
       puts @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
     it "will fail when email is not unique" do 
-      @user1 = User.new(name: "James", email: "hello@example.com", password: "123", password_confirmation: "123")
+      @user1 = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "123", password_confirmation: "123")
       @user1.save
-      @user2 = User.new(name: "James", email: "hello@example.com", password: "123", password_confirmation: "123")
+      @user2 = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "123", password_confirmation: "123")
       @user2.save
       puts @user2.errors.full_messages
       expect(@user2.errors.full_messages).to include("Email has already been taken")
     end
 
     it "will fail when password is not at least 2 characters" do 
-      @user = User.new(name: "James", email: "hello@example.com", password: "1", password_confirmation: "1")
+      @user = User.new(first_name: "James", last_name: "Smith", email: "hello@example.com", password: "1", password_confirmation: "1")
       @user.save
       puts @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 2 characters)")
@@ -68,19 +68,19 @@ RSpec.describe User, type: :model do
     describe '.authenticate_with_credentials' do
       # examples for this class method here
       it "will verify user created is same as user logging in" do
-        @user = User.create(name: "Kyra", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
+        @user = User.create(first_name: "Kyra", last_name: "Smith", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
         @user = User.authenticate_with_credentials("test@test.com", "testpassword")
         expect(@user).to be_truthy
       end
 
       it "will validate with spaces before or after valid email" do
-        @user = User.create(name: "Kyra", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
+        @user = User.create(first_name: "Kyra", last_name: "Smith", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
         @user = User.authenticate_with_credentials("  test@test.com  ", "testpassword")
         expect(@user).to be_truthy
       end
 
       it "will validate with upper and lower case types" do
-        @user = User.create(name: "Kyra", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
+        @user = User.create(first_name: "Kyra", last_name: "Smith", email: "test@test.com", password: "testpassword", password_confirmation: "testpassword")
         @user = User.authenticate_with_credentials("test@TEST.com", "testpassword")
         expect(@user).to be_truthy
       end
